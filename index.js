bc2b769a3d867325dfc3173870f473cb90898303
vijayasagarMantapudi <vijaysagar.mvs@gmail.com>
2020-04-18 19:33:09 +0530

    mongo db used.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const homeRoute = require('./routes/homePage')
 const genres = require('./routes/genres')
 
@@ -9,3 +9,9 @@ vidlyApp.use('/vidly', homeRoute)
 vidlyApp.use('/vidly/api/genres', genres)
 
 vidlyApp.listen(3000)
+console.log('** listening on 3000')
+
+
+ mongoose.connect('mongodb://localhost/vidly')
+ .then(() => {console.log('connected to vidly db')})
+ .catch((err) => {console.log('Error connecting to db:', err)})
--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,38 @@ const express = require('express')
 const fs = require('fs');
 const Joi = require('joi')
 const _ = require('lodash')
+const mongoose = require('mongoose')
+// MongoDB and CRUD ops
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    }
+})
+
+const Genre = mongoose.model('Genre', genreSchema)
+
+async function fetchAllGenres() {
+    let result = await Genre.find()
+    return result
+}
+
+async function fetchGenreById(id) {
+    return await Genre.findById(id)
+}
+
+async function fetchGenreByIdAndUpdate(id, genre) {
+    return await Genre.findByIdAndUpdate(id, genre, { new: true})
+}
+
+async function createGenre(genre) {
+    return await Genre.create(genre)
+}
+
+async function deleteGenre(id) {
+    return await Genre.findByIdAndRemove(id)
+}
 
 function validateGenre(genre) {
     const schema = {
@@ -11,47 +43,85 @@ function validateGenre(genre) {
 }
 const route = express.Router()
 
-route.get('/', (req, res) => {
-    return res.send(fetchroute())
+route.get('/', async (req, res) => {
+    //return res.send(fetchroute())
+    let result
+    try {
+        result = await fetchAllGenres()
+    } catch (err) {
+        console.log('Error while fetching all genres', err)
+    }
+    return res.send(result)
 })
 
-route.get('/:id', (req, res) => {
-    const genre = _.find(fetchroute(), { id: Number(req.params.id) })
-    if (!genre) return res.status(400).send('Genre not found with id: ' + req.params.id)
-    return res.send(genre)
+route.get('/:id', async (req, res) => {
+    //const genre = _.find(fetchroute(), { id: Number(req.params.id) })
+    let result
+    if (!req.params.id) {
+        console.log('** Error: no req.params.id found, req.params: ', req.params)
+    }
+    try {
+        result = await fetchGenreById(req.params.id)
+    } catch (err) {
+        console.log('Error while fetching a genres', err)
+    }
+    //if (!genre) return res.status(400).send('Genre not found with id: ' + req.params.id)
+    return res.send(result)
 })
 
-route.post('/', (req, res) => {
+route.post('/', async (req, res) => {
     const genre = {
         name: req.body.name
     }
     const validation = validateGenre(genre)
     if (validation.error) return  res.status(404).send(validation.error.details[0].message)
-    let genres = fetchroute()
-    genre.id= generateId()
-    route.push(genre)
-    updateroute(genres)
-    return res.send(genre)
+    // let genres = fetchroute()
+    // genre.id= generateId()
+    // route.push(genre)
+    // updateroute(genres)
+    try {
+        result = await createGenre(genre)
+    } catch (err) {
+        console.log('Error while posting a genres', err)
+    }
+    console.log(result)
+    return res.send(result)
 })
 
-route.put('/:id', (req, res) => {
-    let genres = fetchroute()
-    let genre = _.find(genres, { id: Number(req.params.id) })
-    if (!genre) return res.status(400).send('Genre not found with id: ' + req.params.id)
+route.put('/:id', async (req, res) => {
+    // let genres = fetchroute()
+    // let genre = _.find(genres, { id: Number(req.params.id) })
+    // if (!genre) return res.status(400).send('Genre not found with id: ' + req.params.id)
+    let genre = {}
+    let result
     genre.name = req.body.name
     const validation = validateGenre(req.body)
     if (validation.error) return  res.status(404).send(validation.error.details[0].message)
-    updateroute(genres)
-    return res.send(genre)
+    //updateroute(genres)
+    try {
+        result = await fetchGenreByIdAndUpdate(req.params.id, genre)    
+    } catch (err) {
+        console.log('Error while updating a genres', err.message)
+    }
+    return res.send(result)
 })
 
-route.delete('/:id', (req, res) => {
-    let genres = fetchroute()
-    const genreIndex = _.findIndex(genres, { id: Number(req.params.id) })
-    if (genreIndex < 0) return res.status(400).send('Genre not found with id: ' + req.params.id)
-    const genre = route.splice(genreIndex, 1)
-    updateroute(genres)
-    return res.send(genre)
+route.delete('/:id', async (req, res) => {
+    // let genres = fetchroute()
+    // const genreIndex = _.findIndex(genres, { id: Number(req.params.id) })
+    // if (genreIndex < 0) return res.status(400).send('Genre not found with id: ' + req.params.id)
+    // const genre = route.splice(genreIndex, 1)
+    // updateroute(genres)
+    let result
+    if (!req.params.id) {
+        console.log('** Error: no req.params.id found, req.params: ', req.params)
+    }
+    try {
+        result = await deleteGenre(req.params.id)    
+    } catch (err) {
+        console.log('Error while deleting a genres', err)
+    }
+    return res.send(result)
 })
 
 function fetchroute() {
